f7435ca9d1fec70450c258e76f8e935d3d3c37c1
han96 <han960409@naver.com>
2026-05-31 20:06:10 +0900

    123

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,27 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    allowedHosts: [
-      "cityless-lapsible-marianna.ngrok-free.dev",
-    ],
-
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-
-      "/oauth2": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-
-      "/login": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-    },
-  },
+  base: "/WhenWeMeet/",
 });
